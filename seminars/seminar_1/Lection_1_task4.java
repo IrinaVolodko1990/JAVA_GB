2cbddd06dc3affacc3290dded0ca5772d9dbad08
IrinaVolodko1990 <69187493+IrinaVolodko1990@users.noreply.github.com>
2024-01-24 11:32:40 +0300

    Add files via upload
    Add sem_1

---
+++ seminars/seminar_1/Lection_1_task4.java
@@ -0,0 +1,18 @@
+package ru.geekbrains.OOP.seminars.seminar_1;
+
+
+//Во фразе "Добро пожаловать на курс по Java" переставить слова в обратном порядке
+
+
+public class Lection_1_task4 {
+    public static void main(String[] args) {
+        String userStr = "Добро пожаловать на курс по Java";
+        String [] arrStr = userStr.split(" ");
+        String reverseArr = "";
+        for (int i = arrStr.length - 1; i >= 0 ; i--) {
+            reverseArr += arrStr[i] + " ";
+        }
+        System.out.println(reverseArr.trim());
+    }
+
+}
